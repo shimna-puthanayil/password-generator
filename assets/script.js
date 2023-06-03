47905b7530d8b118e559f5329affa9f6e96f966f
shimna <chimmu.p@gmail.com>
2023-06-04 00:54:44 +1000

    created a random password

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,95 @@ var lowercaseSelected = false;
 var uppercaseSelected = false;
 var numericSelected = false;
 var specialSelected = false;
+// Array of special characters to be included in password
+var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+  ];
+  
+  // Array of numeric characters to be included in password
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  // Array of lowercase characters to be included in password
+  var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  
+  // Array of uppercase characters to be included in password
+  var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
 
 function confirmCharacter() {
     lowercaseSelected = window.confirm("Click OK to confirm including lowercase letters?");
@@ -33,7 +122,7 @@ function generatePassword() {
     }
     else {
         confirmCharacter();
-//Confirmed character types are added into an array which will be used to ensure at least one character from each type will be selected.
+        //Confirmed character types are added into an array which will be used to ensure at least one character from each type will be selected.
         if (lowercaseSelected) {
             selectedCharacters.push('L');
         }
@@ -46,7 +135,44 @@ function generatePassword() {
         if (specialSelected) {
             selectedCharacters.push('S');
         }
+        var randomPassword = "";
+        var charArrayLength = 0;
+        for (i = 0; i < passwordLength; i++) {
+            if (charArrayLength === 0) {
+                characterTypes = selectedCharacters;
+                charArrayLength = selectedCharacters.length;
+            }
+            var index = Math.floor(Math.random() * characterTypes.length);
+            randomCharacter = characterTypes[index];
+            characterTypes = characterTypes.toSpliced(index, 1);
+            charArrayLength--;
+            if (randomCharacter === 'L') {
+                randomLowercase = lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
+                randomPassword = randomPassword + randomLowercase;
+            }
+            else if (randomCharacter === 'U') {
+                randomUppercase = upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
+                randomPassword = randomPassword + randomUppercase;
+            }
+            else if (randomCharacter === 'N') {
+                randomNumeric = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+                randomPassword = randomPassword + randomNumeric;
+
+            }
+            else if (randomCharacter === 'S') {
+                randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+                randomPassword = randomPassword + randomSpecial;
+
+            }
+            else {
+                window.alert("Please select at least one character type");
+                return;
+            }
+
+
+        }
     }
+    return randomPassword;
 };
 
 // Get references to the #generate element
