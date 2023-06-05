d100864d2ab25f31095e1b2e032f0dc38767a190
shimna <chimmu.p@gmail.com>
2023-06-05 14:41:02 +1000

    created an object

--- assets/script.js
+++ assets/script.js
@@ -3,98 +3,8 @@ var lowercaseSelected = false;
 var uppercaseSelected = false;
 var numericSelected = false;
 var specialSelected = false;
-var character;
-// Array of special characters to be included in password
-var specialCharacters = [
-    '@',
-    '%',
-    '+',
-    '\\',
-    '/',
-    "'",
-    '!',
-    '#',
-    '$',
-    '^',
-    '?',
-    ':',
-    ',',
-    ')',
-    '(',
-    '}',
-    '{',
-    ']',
-    '[',
-    '~',
-    '-',
-    '_',
-    '.'
-];
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-];
-
-// This function validates the character types that are confirmed for generting the password. 
+// This function validates the character types that are confirmed for generating the password. 
 // Checks whether at least one character type is confirmed.
 
 function confirmCharacter() {
@@ -124,21 +34,51 @@ function confirmCharacter() {
     return confirmed;
 }
 
-//This function selects and returns a random character from an array which is passed as parameter of the function.
+//An object is created which contains all character arrays and functions to generate a random character.
+var randomObject = {
+    character: "",
+    randomChar: "",
+    // Array of special characters
+    specialCharacters: ["@", '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'],
+    // Array of lowercase characters
+    lowerCasedCharacters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    // Array of uppercase characters
+    upperCasedCharacters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    // Array of numeric characters
+    numericCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+   
+    getrandomCharacter: function (characterArray) {// function to get a random character from an array which is passed as parameter.
+        this.character = characterArray[Math.floor(Math.random() * characterArray.length)];
+        return this.character;
+    },
+    getCharacter: function (randomChartype) {
+        if (randomChartype === 'L') {
+            this.randomChar = this.getrandomCharacter(this.lowerCasedCharacters);//get a character from an array of lowercase letters
+        }
+        else if (randomChartype === 'U') {
+            this.randomChar = this.getrandomCharacter(this.upperCasedCharacters);//get a character from an array of uppercase letters
+        }
+        else if (randomChartype === 'N') {
 
-function getrandomCharacter(characterArray) {
-    character = characterArray[Math.floor(Math.random() * characterArray.length)];
-    return character;
-}
+            this.randomChar = this.getrandomCharacter(this.numericCharacters);//get a character from an array of numeric characters
+        }
+        else if (randomChartype === 'S') {
+            this.randomChar = this.getrandomCharacter(this.specialCharacters);//get a character from an array of special characters
+        }
+        else {
+            return;
+        }
+        return this.randomChar;
+    }
 
+};
 // Generates a random password 
 function generatePassword() {
     var selectedCharacters = [];
     var randomPassword = "";
-    var charArrayLength = 0;
     var characterTypes = [];
     var passwordLength = window.prompt("How many characters would you like your password to contain(8-123)");
-
+    //validation
     if (passwordLength === null) {
         return randomPassword;
     }
@@ -169,40 +109,26 @@ function generatePassword() {
             }
 
             for (i = 0; i < passwordLength; i++) {
-                //selected character type array is copied into another array 'characterTypes' .
+                //selected character type array 'selectedCharacters' is copied into another array 'characterTypes' .
 
                 if (characterTypes.length === 0) {
                     characterTypes = selectedCharacters;
                 }
 
                 //Randomly chooses a character type 'L','U','N' OR 'S' from 'characterTypes' and stored in 'randomCharacter'.
-                //which  is used to get a random character from that character array[lowercase ,uppercase ,numeric or special character arrays]
-                //Then that character type['L','U','N' OR 'S'] is removed from the  same array 'characterTypes'.This ensures next character
-                // will be selected from a different type(next iteration) and at last the password will have random characters from all confirmed types.
+                //which  is used to get a random character from the corresponding character array[lowercase ,uppercase ,numeric or special character arrays]
+                //Then that character type 'L','U','N' OR 'S' is removed from the  same array 'characterTypes'.This ensures next character
+                // will be selected from a different type(in next iteration) and at last the password will have random characters from all confirmed types.
 
 
                 var index = Math.floor(Math.random() * characterTypes.length);
-                randomCharacter = characterTypes[index];
+                randomCharacter = characterTypes[index];//'L','U','N' OR 'S'
 
                 characterTypes = characterTypes.toSpliced(index, 1);
 
-                if (randomCharacter === 'L') {
-                    randomPassword = randomPassword + getrandomCharacter(lowerCasedCharacters);
-                }
-                else if (randomCharacter === 'U') {
-                    randomPassword = randomPassword + getrandomCharacter(upperCasedCharacters);
-                }
-                else if (randomCharacter === 'N') {
+                // Gets the random character from the corresponding character array and appends it with password variable 'randomPassword'
+                randomPassword = randomPassword + randomObject.getCharacter(randomCharacter);
 
-                    randomPassword = randomPassword + getrandomCharacter(numericCharacters);
-                }
-                else if (randomCharacter === 'S') {
-                    randomPassword = randomPassword + getrandomCharacter(specialCharacters);
-                }
-                else {
-
-                    return;
-                }
             }
         }
     }
